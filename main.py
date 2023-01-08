3af6066c4af895b33b562d3fc4918d48bb60bf85
JaehyoJJAng <yshrim12@naver.com>
2023-01-08 13:51:19 +0900

    단위 테스트 : RealTimeSearchWord 단위 테스트 완료

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 from config.config import get_token
-from typing import List,Dict 
+from typing import List,Dict,Union
 import discord
 import os
 import sys
@@ -13,46 +13,34 @@ class Discord:
 
     def return_bot(self):
         """ 봇 객체 리턴 메소드 """
-        prefix = '!'
+        prefix : str = '!'
 
         # intents : 봇이 서버 멤버의 정보나 , 서버 멤버 리스트를 불러올 수 있도록 허용하는 설정이었음
-        intents = discord.Intents.all()
+        intents : discord.Intents.all() = discord.Intents.all()
 
         # Bot 객체 생성
-        client = commands.Bot(command_prefix=prefix,intents=intents)
+        client : commands.Bot(command_prefix=prefix,intents=intents) = commands.Bot(command_prefix=prefix,intents=intents)
         return client
 
-    def load_extensions(self,client):        
+    def load_extensions(self,client)-> Union[None,bool]:
         for filename in os.listdir('cogs/'):
             if '.py' in filename:
                 filename = filename.replace('.py','')
-                client.load_extension(f"cogs.{filename}")
+                try:
+                    client.load_extension(f"cogs.{filename}")
+                except:
+                    return False
 
     def execute_bot(self,client:commands.Bot)-> None:
         """ 봇 실행하는 메소드 """
         client.run(self._token)
-
-    def say_realtime_search_keyword(self,client:commands.Bot)-> None:
-        """ 실시간 검색어 명령 """            
-        @client.command(name='실시간검색어')
-        async def _live(ctx):
-            try :
-                realtime = RealTimeSearchWord()
-                item_list : list = realtime.get_items()
-                for idx,item in enumerate(item_list,1):
-                    title = item['title']
-                    link  = item['link']
-
-                    await ctx.send(f'순위 : {idx}\t검색명 : {title}\t링크 : {link}\n')
-            except:
-                sys.exit()
 
 def main()-> None:
     # Create Discord Instance
     dico = Discord()
 
     # Return Discord Bot Instance
-    client : commands.Bot = dico.return_bot()
+    client = dico.return_bot()
     
     # Load Extensions
     dico.load_extensions(client=client)
---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import os
+import sys
+
+# ====================== sys.path 추가 ======================
+BASE_DIR : str = os.path.dirname(Path(__file__).resolve().parent)
+sys.path.append(BASE_DIR)
+from cogs.real_time_search_word import RealTimeSearchWord
+from main import Discord
+# ======================               ======================
