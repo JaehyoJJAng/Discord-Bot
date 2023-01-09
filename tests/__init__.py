605537312232159db10b27b19f3a848e41fbfd57
JaehyoJJAng <yshrim12@naver.com>
2023-01-09 15:45:01 +0900

    단위 테스트 : homework.py.HomeworkCSV 단위 테스트 완료

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,7 +7,7 @@ BASE_DIR : str = os.path.dirname(Path(__file__).resolve().parent)
 sys.path.append(BASE_DIR)
 from cogs.real_time_search_word import RealTimeSearchWord
 from cogs.lunch import Lunch
-from cogs.homework import HomeWorkLunch
+from cogs.homework import HomeWorkLunch,HomeWorkCSV
 from cogs.quiz import CSV,Quiz
 from main import Discord
 # ======================               ======================
--- tests/test_homework.py
+++ tests/test_homework.py
@@ -1,4 +1,4 @@
-from __init__ import HomeWorkLunch
+from __init__ import HomeWorkLunch,HomeWorkCSV
 from typing import List,Dict
 import os
 import unittest
@@ -26,6 +26,29 @@ class LunchTest(unittest.TestCase):
     def test_random_choice(self)-> None:
         text : str = self.lunch.random_choice()
         self.assertIn('오늘 점심은',text)
+
+class HomeWorkCSVTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls)-> None:
+        cls.csv = HomeWorkCSV()
+
+        cls.file_path : str = r'C:\github\Discord-Bot\cogs\data'
+    
+    def test_get_csv_data(self)-> None:
+        data : Dict[str,str] = self.csv.get_csv_data(file=os.path.join(self.file_path,'quiz.csv'))
+        self.assertEqual(len(data),30)
+
+    def test_get_answer(self)-> None:
+        data : Dict[str,str] = self.csv.get_csv_data(file=os.path.join(self.file_path,'quiz.csv'))
+        
+        problem_info : Dict[str,str] = self.csv.get_answer(quiz_dict=data)
+
+        self.assertIn('problem',list(problem_info.keys()))
+        self.assertIn('answer',list(problem_info.keys()))
+
+    def test_json_to_dict(self)-> None:
+        dic : dict = self.csv.json_to_dict(file=os.path.join(self.file_path,'score.json'))
+        self.assertDictEqual(dic,{'착짱죽짱멸공하자':2})
 
 if __name__ == '__main__':
     unittest.main()
